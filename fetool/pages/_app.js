d21e578a5d69489ef26043817d515505c676821c
as-msys <aperiyakaruppan@msystechnologies.com>
2022-11-14 15:39:13 +0530

    Increased the time limit of toast message

--- fetool/pages/_app.js
+++ fetool/pages/_app.js
@@ -1,12 +1,12 @@
 import "../styles/globals.css";
-import { ToastContainer,toast } from "react-toastify";
+import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { parseCookies  } from 'nookies'
+import { parseCookies } from "nookies";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <ToastContainer autoClose={1000}/>
+      <ToastContainer autoClose={3000} />
       <Component {...pageProps} />
     </>
   );
@@ -15,32 +15,31 @@ function MyApp({ Component, pageProps }) {
 function redirectUser(ctx, location) {
   if (ctx.req) {
     //302-Resource requested has been temporarily moved to the URL given by the Location header.
-      ctx.res.writeHead(302, { Location: location });
-      ctx.res.end();
+    ctx.res.writeHead(302, { Location: location });
+    ctx.res.end();
   } else {
-      Router.push(location);
+    Router.push(location);
   }
 }
 
-MyApp.getInitialProps = async ({Component, ctx}) => {
-  let pageProps = {}
+MyApp.getInitialProps = async ({ Component, ctx }) => {
+  let pageProps = {};
   //Accessing cookies in NextJS from server side
-  const jwt = parseCookies(ctx).jwt
+  const jwt = parseCookies(ctx).jwt;
 
   if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx)
+    pageProps = await Component.getInitialProps(ctx);
   }
 
   if (!jwt) {
-      if (ctx.pathname === "/clients") {
-        redirectUser(ctx, "/");
-      }
+    if (ctx.pathname === "/clients") {
+      redirectUser(ctx, "/");
+    }
   }
 
   return {
-      pageProps,
-     
-  }
-}
+    pageProps,
+  };
+};
 
 export default MyApp;
