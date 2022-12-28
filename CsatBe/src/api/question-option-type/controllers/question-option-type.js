ed8c23851388fd35af37b65683e8c2ab9b2256c0
as-msys <aperiyakaruppan@msystechnologies.com>
2022-12-28 13:06:22 +0530

    structure chnage

--- CsatBe/src/api/question-option-type/controllers/question-option-type.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * question-option-type controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::question-option-type.question-option-type');
--- CsatBe/src/api/question-type/services/question-type.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * question-type service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::question-type.question-type');
